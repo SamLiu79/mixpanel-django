dd019df498bd8cd5bc1c3fc48e1ca9321fd57989
sam <devnull@localhost>
2010-12-23 23:46:02 +0800

    fix js styles

--- mixpanel_django/media/scripts/mp_common_events.js
+++ mixpanel_django/media/scripts/mp_common_events.js
@@ -1,40 +1,40 @@
 // common event functions start here
-var trackMixpanelEvent = function(){
-    var null_fn = function(){
+var trackMixpanelEvent = function() {
+    var null_fn = function() {
     };
-    
+
     switch (arguments.length) {
         case 2:
             var event_name = arguments[0];
             var event_data = arguments[1];
             var event_callback = null_fn;
-            break
+            break;
         case 3:
             var event_name = arguments[0];
             var event_data = arguments[1];
             var event_callback = arguments[2];
-            break
+            break;
         default:
-            throw "mixpanel args error"
+            throw 'mixpanel args error';
     }
-    
-    switch (MP_LOAD_TYPE) {
-        case "async":
-            mpq.push(["track", event_name, event_data]);
-            break
-        case "standard":
-            mpmetrics.track(event_name, event_data, event_callback);
-            break
-        case "fbml":
-            mpmetrics.track(event_name, event_data)
-            break
-        default:
-            throw "mixpanel type error"
-    }
-}
 
-var trackfunnelMixpanelEvent = function(){
-    var null_fn = function(){
+    switch (MP_LOAD_TYPE) {
+        case 'async':
+            mpq.push(['track', event_name, event_data]);
+            break;
+        case 'standard':
+            mpmetrics.track(event_name, event_data, event_callback);
+            break;
+        case 'fbml':
+            mpmetrics.track(event_name, event_data);
+            break;
+        default:
+            throw 'mixpanel type error';
+    }
+};
+
+var trackfunnelMixpanelEvent = function() {
+    var null_fn = function() {
     };
     switch (arguments.length) {
         case 4:
@@ -43,31 +43,34 @@ var trackfunnelMixpanelEvent = function(){
             var funnel_goal = arguments[2];
             var funnel_data = arguments[3];
             var funnel_callback = null_fn;
-            break
+            break;
         case 5:
             var funnel_name = arguments[0];
             var funnel_step = arguments[1];
             var funnel_goal = arguments[2];
             var funnel_data = arguments[3];
             var funnel_callback = arguments[4];
-            break
+            break;
         default:
-            throw "mixpanel args error"
+            throw 'mixpanel args error';
     }
-    
+
     switch (MP_LOAD_TYPE) {
-        case "async":
-            mpq.push(["track_funnel", funnel_name, funnel_step, funnel_goal, funnel_data]);
-            break
-        case "standard":
-            mpmetrics.track_funnel(funnel_name, funnel_step, funnel_goal, funnel_data, funnel_callback);
-            break
-        case "fbml":
-            mpmetrics.track_funnel(funnel_name, funnel_step, funnel_goal, funnel_data);
-            break
+        case 'async':
+            mpq.push(['track_funnel', funnel_name, funnel_step,
+                funnel_goal, funnel_data]);
+            break;
+        case 'standard':
+            mpmetrics.track_funnel(funnel_name, funnel_step,
+                funnel_goal, funnel_data, funnel_callback);
+            break;
+        case 'fbml':
+            mpmetrics.track_funnel(funnel_name, funnel_step,
+                funnel_goal, funnel_data);
+            break;
         default:
-            throw "mixpanel type error"
+            throw 'mixpanel type error';
     }
-}
+};
 
 
